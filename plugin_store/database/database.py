a8a367731f0fe17e331424dee09121525ea20b53
AAGaming <aa@mail.catvibers.me>
2022-07-19 02:21:20 +0000

    specify it twice????

--- plugin_store/database/database.py
+++ plugin_store/database/database.py
@@ -99,9 +99,9 @@ class Database:
             return None
     
     async def delete_plugin(self, id):
-        query = delete(PluginTag).filter(PluginTag.c.artifact_id == id)
+        query = delete(PluginTag).where(PluginTag.c.artifact_id == id)
         print(query)
-        await self.session.execute(query)
+        await self.session.execute(query, artifact_id_1=id)
         await self.session.execute(delete(Version).where(Version.artifact_id == id))
         await self.session.execute(delete(Artifact).where(Artifact.id == id))
         return await self.session.commit()
