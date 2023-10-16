402e61f5e6278eb195dfb93676f3394020e6bf37
marios8543 <marios8543@gmail.com>
2023-10-17 01:17:34 +0300

    add cdn retry logic

--- plugin_store/constants.py
+++ plugin_store/constants.py
@@ -5,6 +5,7 @@ BASE_DIR = Path(__file__).expanduser().resolve().parent
 TEMPLATES_DIR = BASE_DIR / "templates"
 
 CDN_URL = "https://cdn.tzatzikiweeb.moe/file/steam-deck-homebrew/"
+CDN_ERROR_RETRY_TIMES = 5
 
 
 class SortDirection(Enum):
