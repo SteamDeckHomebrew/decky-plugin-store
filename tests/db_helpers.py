e61d2178a15177fc12f0ff2e2ff7b889314cadad
GwynBleidD <gbd.lin@gmail.com>
2024-01-13 20:36:18 +0100

    refactor: Use transactional DB tests
    Make use of DB transactions to rollback tests back to the original state
    instead of recreating DB on each test.

--- tests/db_helpers.py
+++ tests/db_helpers.py
@@ -1,11 +1,16 @@
 import random
+
+from contextlib import asynccontextmanager
 from datetime import datetime, timedelta, UTC
 from hashlib import sha256
+from os import getenv
 
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import event
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 
-from database.models import Artifact, Tag, Version
+from database.models import Artifact, Base, Tag, Version
 
 
 class FakePluginGenerator:
@@ -41,7 +46,7 @@ class FakePluginGenerator:
         image_path,
         tags,
         visible,
-        id=None,
+        id_=None,
     ):
         plugin = Artifact(
             name=name,
@@ -51,8 +56,8 @@ class FakePluginGenerator:
             tags=tags,
             visible=visible,
         )
-        if id is not None:
-            plugin.id = id
+        if id_ is not None:
+            plugin.id = id_
         self.session.add(plugin)
         await self.session.commit()
         return plugin
@@ -107,3 +112,80 @@ class FakePluginGenerator:
         await self._create_versions(plugin, versions)
 
         self.created_plugins_count += 1
+
+
+def create_test_db_engine() -> "AsyncEngine":
+    return create_async_engine(
+        getenv("DB_URL"),
+        pool_pre_ping=True,
+        # echo=True,
+    )
+
+
+def create_test_db_sessionmaker(engine: "AsyncEngine") -> "sessionmaker":
+    return sessionmaker(
+        bind=engine,
+        autoflush=False,
+        future=True,
+        expire_on_commit=False,
+        autocommit=False,
+        class_=AsyncSession,
+    )
+
+
+async def migrate_test_db(engine: "AsyncEngine") -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def seed_test_db(db_sessionmaker: "sessionmaker") -> None:
+    session = db_sessionmaker()
+    generator = FakePluginGenerator(session, datetime(2022, 2, 25, 0, 0, 0, tzinfo=UTC))
+    await generator.create(tags=["tag-1", "tag-2"], versions=["0.1.0", "0.2.0", "1.0.0"])
+    generator.date = datetime(2022, 2, 25, 0, 1, 0, 0, tzinfo=UTC)
+    await generator.create(image="2.png", tags=["tag-2"], versions=["1.1.0", "2.0.0"])
+    generator.date = datetime(2022, 2, 25, 0, 2, 0, 0, tzinfo=UTC)
+    await generator.create("third", tags=["tag-2", "tag-3"], versions=["3.0.0", "3.1.0", "3.2.0"])
+    generator.date = datetime(2022, 2, 25, 0, 3, 0, 0, tzinfo=UTC)
+    await generator.create(tags=["tag-1", "tag-3"], versions=["1.0.0", "2.0.0", "3.0.0", "4.0.0"])
+    generator.date = datetime(2022, 2, 25, 0, 4, 0, 0, tzinfo=UTC)
+    await generator.create(tags=["tag-1", "tag-2"], versions=["0.1.0", "0.2.0", "1.0.0"], visible=False)
+    generator.date = datetime(2022, 2, 25, 0, 5, 0, 0, tzinfo=UTC)
+    await generator.create(image="6.png", tags=["tag-2"], versions=["1.1.0", "2.0.0"], visible=False)
+    generator.date = datetime(2022, 2, 25, 0, 6, 0, 0, tzinfo=UTC)
+    await generator.create("seventh", tags=["tag-2", "tag-3"], versions=["3.0.0", "3.1.0", "3.2.0"], visible=False)
+    generator.date = datetime(2022, 2, 25, 0, 7, 0, 0, tzinfo=UTC)
+    await generator.create(tags=["tag-1", "tag-3"], versions=["1.0.0", "2.0.0", "3.0.0", "4.0.0"], visible=False)
+
+
+async def prepare_test_db(
+    engine: "AsyncEngine",
+    db_sessionmaker: "sessionmaker",
+    seed: bool = False,
+) -> None:
+    await migrate_test_db(engine)
+    if seed:
+        await seed_test_db(db_sessionmaker)
+
+
+@asynccontextmanager
+async def prepare_transactioned_db_session(engine: "AsyncEngine", db_sessionmaker: "sessionmaker") -> "AsyncSession":
+    connection = await engine.connect()
+    outer_transaction = await connection.begin()
+    async_session = db_sessionmaker(bind=connection)
+    # seems like for sqlite releasing last savepoint commits the whole transaction. This should fix that.
+    await connection.begin_nested()
+    nested = await connection.begin_nested()
+
+    @event.listens_for(async_session.sync_session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        nonlocal nested
+
+        if not nested.is_active:
+            nested = connection.sync_connection.begin_nested()
+
+    yield async_session
+
+    await outer_transaction.rollback()
+    await async_session.close()
+    await connection.close()
